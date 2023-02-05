e98fdbbc72f944fac53ca724cf2def9c533b21f1
IAmAsianNoob <>
2023-02-05 11:10:04 +0100

    Delimit multiple filters with commas

--- amqUserFilter.user.js
+++ amqUserFilter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ user filter
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Adds an input field that allows you to filter friends and all users
 // @author       IAmAsianNoob
 // @match        https://animemusicquiz.com/*
@@ -16,9 +16,9 @@ const ORIGINAL_NAMES = JSON.parse(localStorage.getItem('AMQOriginalPlayerNames')
 
 function onInput(e) {
 	if (e.inputType === 'insertText' || e.inputType === 'deleteContentBackward' || e.inputType === 'deleteContentForward') {
-		const filter = e.target.value.toLowerCase();
-		if (filter) {
-			applyFilter(filter);
+		const filters = e.target.value.toLowerCase().split(',').map(f => f.trim()).filter(s => s);
+		if (filters.length) {
+			applyFilter(filters);
 		} else {
 			showAllUsers();
 		}
@@ -32,11 +32,11 @@ function showAllUsers() {
 	}
 }
 
-function applyFilter(filter) {
+function applyFilter(filters) {
 	const allPlayers = [...Object.entries(socialTab.onlineFriends), ...Object.entries(socialTab.offlineFriends), ...Object.entries(socialTab.allPlayerList._playerEntries)];
 	for (const [playerName, player] of allPlayers) {
 		const originalName = ORIGINAL_NAMES[playerName] ?? '';
-		if (playerName.toLowerCase().indexOf(filter) !== -1 || originalName.toLowerCase().indexOf(filter) !== -1) {
+		if (filters.some(f => playerName.toLowerCase().indexOf(f) !== -1) || filters.some(f => originalName.toLowerCase().indexOf(f) !== -1)) {
 			player.show?.() || player.$html.show();
 		} else {
 			player.hide?.() || player.$html.hide();
