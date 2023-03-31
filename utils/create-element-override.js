0586cf767099b9d2d424153e37b054e8a3b087f2
IAmAsianNoob <>
2023-03-31 15:08:23 +0200

    Create exceptions for the textContent and checked properties

--- utils/create-element-override.js
+++ utils/create-element-override.js
@@ -2,13 +2,18 @@ if (!document) return;
 
 // I do not perform any validity checks, that's on yourself.
 document.createElement = function(_super) {
+	const fieldExceptions = new Set(['textContent', 'checked']);
 	return function() {
 		const element = _super.apply(this, arguments);
 		const [, options] = arguments;
 		if (options && typeof options === 'object') {
 			for (const [field, value] of Object.entries(options)) {
 				if (field === 'is') return;
-				element.setAttribute(field, value);
+				if (fieldExceptions.has(field)) {
+					element[field] = value;
+				} else {
+					element.setAttribute(field, value);
+				}
 			}
 		}
 		return element;
